ba63c584da7ea5475f450dc63bba2fa5bbdb3f42
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-01-29 10:47:23 +0530

    Express server and body-parser

--- index.js
+++ index.js
@@ -1,86 +1,37 @@
-const http = require("http");
-const fs = require("fs");
-
-// Create a local server to receive data from
-/**
- * req - Request which is coming from the client
- * res - Response that we are sending to client
- */
+const express = require("express");
+const body = require("express");
+const bodyparser = require("body-parser");
+const HTTP_SERVER = express();
 const PORT = 5000;
-http
-  .createServer((req, res) => {
-    if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(`
-            <html>
-                <head>
-                    <title>Home</title>
-                </head>
-                <body>
-                    <h1>Welcome to basic node application</h1>
-                </body>
-            </html>
-            `);
-    } else if (req.url === "/about") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(`
-            <html>
-                <head>
-                    <title>About Us</title>
-                </head>
-                <body>
-                    <h1>About us page</h1>
-                </body>
-            </html >
-            `);
-    } else if (req.url === "/contact") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(`
-            <html>
-                <head>
-                    <title>Contact us</title>
-                </head>
-                <body>
-                    <h1>Contact Us Page</h1>
-                </body>
-            </html >
-            `);
-    } else if ("/favicon.ico") {
-      const image = fs.readFileSync("./assets/web.png");
-      res.writeHead(200, {
-        "Content-Type": "image/png",
-        "Content-Length": image.length,
-      });
-      // Send the body of the response
-      res.end(image);
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(`
-            <html>
-                <head>
-                    <title>404 Not Found</title>
-                </head>
-                <body>
-                    <h1>404 NOT FOUND</h1>
-                </body>
-            </html >
-            `);
-    }
-  })
-  .listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
-  });
 
-// res.writeHead(200, { "Content-Type": "application/json" });
-// res.write(`<html>
-//                 <head>
-//                     <title>Test</title></head>
-//                 <body>
-//                     <h1>Hello all</h1>
-//                 </body>
-//             </html >`);
-// res.end(
-//   JSON.stringify({
-//     data: "Hello World!",
-//   })
-// );
+// MVC - MODEL VIEW CONTROLLER
+// MVRC - MODEL VIEW ROUTER CONTROLLER
+
+// INJECTING MIDDLE-WARE
+HTTP_SERVER.use(bodyparser.json());
+
+HTTP_SERVER.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
+
+HTTP_SERVER.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Successful!!",
+  });
+});
+
+HTTP_SERVER.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    message: "Post request hit",
+  });
+});
+
+HTTP_SERVER.get("/user/:id/:type", (req, res) => {
+  const param = req.params;
+  const queryParams = req.query;
+  console.log(param, queryParams);
+  res.status(200).json({
+    message: "GET WITH ID request hit",
+  });
+});
