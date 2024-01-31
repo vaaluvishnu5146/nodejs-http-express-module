892c5734815914099fb1895b1a337f54d79c0856
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-01-31 08:46:56 +0530

    Added Dotenv config

--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,12 @@
 const { default: mongoose } = require("mongoose");
 
 async function connectDatabase() {
+  const uri =
+    process.env.NODE_ENV === "development"
+      ? `${process.env.MONGODB_DEV_URI}${process.env.MONGODB_DEV_NAME}`
+      : `${process.env.MONGODB_PROD_URI}${process.env.MONGODB_PROD_NAME}`;
   try {
-    const response = await mongoose.connect(
-      "mongodb://localhost:27017/bookings"
-    );
+    const response = await mongoose.connect(uri);
     if (response.connections.length > 0) {
       console.log("Database connection successful");
     } else {
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const HTTP_SERVER = express();
 const PORT = 5000;
 const { connectDatabase } = require("./dbconfig");
 
+// ENABLING APPLICATION LEVEL ENV
+require("dotenv").config();
+
 connectDatabase();
 // MVC - MODEL VIEW CONTROLLER
 // MVRC - MODEL VIEW ROUTER CONTROLLER
@@ -12,7 +15,7 @@ connectDatabase();
 // INJECTING MIDDLE-WARE
 HTTP_SERVER.use(bodyparser.json());
 
-HTTP_SERVER.listen(PORT, () => {
+HTTP_SERVER.listen(PORT, process.env.HOSTNAME, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 });
 
